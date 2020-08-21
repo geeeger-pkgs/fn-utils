f5bbb79ecb73141b999beabc4216fea587a6cfaf
geeeger <lph1988520@sina.com>
2020-08-21 11:09:40 +0800

    feat(fn-empty-class): add package

---
+++ packages/fn-empty-class/__tests__/index.spec.ts
@@ -0,0 +1,7 @@
+import EmptyClass from '../src';
+
+describe('fn-empty-class module', () => {
+  it('should defined', () => {
+    expect(new EmptyClass()).toBeDefined();
+  });
+});
---
+++ packages/fn-empty-class/src/index.ts
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export default class EmptyClass {
+  constructor(config?: any) {
+    // nothingtodo
+  }
+}
